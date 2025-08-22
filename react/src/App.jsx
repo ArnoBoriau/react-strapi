b51358a964882f670f6783b1b37596ddf64895b1
ArnoBoriau <arno_boriau@icloud.com>
2025-08-22 17:01:42 +0200

    reset db. Created new Home content type with title and bodycopy attribute

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,28 +1,28 @@
 import React from "react";
 
 function App() {
-  const [articles, setArticles] = React.useState([]);
+  const [home, setHome] = React.useState([]);
   const STRAPI_URL = "http://localhost:1337";
 
-  const fetchArticles = async () => {
-    const response = await fetch(`${STRAPI_URL}/api/articles?populate=*`);
+  const fetchHome = async () => {
+    const response = await fetch(`${STRAPI_URL}/api/home`);
     const data = await response.json();
     console.log(data);
     return data.data;
   };
 
   React.useEffect(() => {
-    fetchArticles().then(setArticles);
+    fetchHome().then(setHome);
   }, []);
 
   return (
     <>
-      {articles.map((article) => (
-        <div key={article.id}>
-          <h2>{article.title}</h2>
-          <p>{article.description}</p>
+      {home && (
+        <div key={home.id} className={`${home.id}`}>
+          <h2>{home.title}</h2>
+          <p>{home.bodycopy}</p>
         </div>
-      ))}
+      )}
     </>
   );
 }
