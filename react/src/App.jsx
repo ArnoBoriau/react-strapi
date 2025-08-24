9004f68a8737e673d31d3b6690ca5705817a6683
ArnoBoriau <arno_boriau@icloud.com>
2025-08-24 11:54:02 +0200

    Updated Stapi content to home and test data. Still fake btw. Also called both in react.

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -2,10 +2,18 @@ import React from "react";
 
 function App() {
   const [home, setHome] = React.useState([]);
+  const [test, setTest] = React.useState([]);
   const STRAPI_URL = "http://localhost:1337";
 
   const fetchHome = async () => {
-    const response = await fetch(`${STRAPI_URL}/api/home`);
+    const response = await fetch(`${STRAPI_URL}/api/home?populate=*`);
+    const data = await response.json();
+    console.log(data);
+    return data.data;
+  };
+
+  const fetchTest = async () => {
+    const response = await fetch(`${STRAPI_URL}/api/test?populate=*`);
     const data = await response.json();
     console.log(data);
     return data.data;
@@ -13,14 +21,22 @@ function App() {
 
   React.useEffect(() => {
     fetchHome().then(setHome);
+    fetchTest().then(setTest);
   }, []);
 
   return (
     <>
-      {home && (
-        <div key={home.id} className={`${home.id}`}>
-          <h2>{home.title}</h2>
-          <p>{home.bodycopy}</p>
+      {test && (
+        <div key={test.id} className={`${test.id}`}>
+          <h2>{test.title}</h2>
+          <p>{test.bodycopy}</p>
+        </div>
+      )}
+      {home.header && (
+        <div>
+          <h3>{home.header.title}</h3>
+          <p>{home.header.bodycopy}</p>
+          <button>{home.header.btn}</button>
         </div>
       )}
     </>
--- server/types/generated/components.d.ts
+++ server/types/generated/components.d.ts
@@ -1,3 +1,33 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface PageHeader extends Struct.ComponentSchema {
+  collectionName: 'components_page_headers';
+  info: {
+    displayName: 'Header';
+  };
+  attributes: {
+    bodycopy: Schema.Attribute.Text;
+    btn: Schema.Attribute.String;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface PageSection extends Struct.ComponentSchema {
+  collectionName: 'components_page_sections';
+  info: {
+    displayName: 'Section';
+  };
+  attributes: {
+    bodycopy: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'page.header': PageHeader;
+      'page.section': PageSection;
+    }
+  }
+}
